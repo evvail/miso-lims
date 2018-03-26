a2899283a0a9344e680df8bf3238e03099a7eb2b
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-03-26 16:55:44 -0400

    Sketch implementation for makeProgressStep

--- core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/workflow/ProgressStep.java
+++ core/src/main/java/uk/ac/bbsrc/tgac/miso/core/data/workflow/ProgressStep.java
@@ -2,9 +2,12 @@ package uk.ac.bbsrc.tgac.miso.core.data.workflow;
 
 import java.io.Serializable;
 
+import uk.ac.bbsrc.tgac.miso.core.data.Barcodable;
+import uk.ac.bbsrc.tgac.miso.core.data.workflow.impl.IntegerProgressStep;
+import uk.ac.bbsrc.tgac.miso.core.data.workflow.impl.PoolProgressStep;
+
 /**
- * Holds the data for a single workflow step
- * Each input should have its own step
+ * Holds the data for a single workflow step Each input should have its own step
  */
 public interface ProgressStep extends Serializable, Comparable<ProgressStep> {
   Progress getProgress();
@@ -16,13 +19,40 @@ public interface ProgressStep extends Serializable, Comparable<ProgressStep> {
   void setStepNumber(int stepNumber);
 
   /**
-   * Part of the Visitor Pattern to use WorkflowStep to validate ProgressStep
-   * All implementations of this method should call {@code visitor.processInput(this)}
-   * @param visitor WorkflowStep used to validate {@code this}
+   * Part of the Visitor Pattern to use WorkflowStep to validate ProgressStep All implementations of this method should call
+   * {@code visitor.processInput(this)}
+   * 
+   * @param visitor
+   *          WorkflowStep used to validate {@code this}
    */
   void accept(WorkflowStep visitor);
 
   enum InputType {
-    SEQUENCERPARTITIONCONTAINER, POOL, PLATFORM, INTEGER
+    POOL {
+      @Override
+      public ProgressStep createStep() {
+        return new PoolProgressStep();
+      }
+
+      @Override
+      public boolean isBarcodable() {
+        return true;
+      }
+    },
+    INTEGER {
+      @Override
+      public ProgressStep createStep() {
+        return new IntegerProgressStep();
+      }
+
+      @Override
+      public boolean isBarcodable() {
+        return false;
+      }
+    };
+
+    public abstract ProgressStep createStep();
+
+    public abstract boolean isBarcodable();
   }
 }
--- miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultWorkflowManager.java
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/impl/DefaultWorkflowManager.java
@@ -3,6 +3,8 @@ package uk.ac.bbsrc.tgac.miso.service.impl;
 import static uk.ac.bbsrc.tgac.miso.core.data.workflow.Workflow.WorkflowName;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -12,14 +14,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sun.xml.internal.ws.developer.MemberSubmissionAddressing;
+
+import uk.ac.bbsrc.tgac.miso.core.data.Barcodable;
+import uk.ac.bbsrc.tgac.miso.core.data.Barcodable.EntityType;
+import uk.ac.bbsrc.tgac.miso.core.data.impl.view.BarcodableView;
 import uk.ac.bbsrc.tgac.miso.core.data.workflow.Progress;
 import uk.ac.bbsrc.tgac.miso.core.data.workflow.ProgressStep;
 import uk.ac.bbsrc.tgac.miso.core.data.workflow.ProgressStep.InputType;
 import uk.ac.bbsrc.tgac.miso.core.data.workflow.Workflow;
+import uk.ac.bbsrc.tgac.miso.core.data.workflow.impl.IntegerProgressStep;
 import uk.ac.bbsrc.tgac.miso.core.data.workflow.impl.LoadSequencerWorkflow;
 import uk.ac.bbsrc.tgac.miso.core.data.workflow.impl.ProgressImpl;
 import uk.ac.bbsrc.tgac.miso.core.store.ProgressStore;
+import uk.ac.bbsrc.tgac.miso.service.BarcodableViewService;
 import uk.ac.bbsrc.tgac.miso.service.WorkflowManager;
+import uk.ac.bbsrc.tgac.miso.service.exception.ValidationError;
+import uk.ac.bbsrc.tgac.miso.service.exception.ValidationException;
+import uk.ac.bbsrc.tgac.miso.service.exception.ValidationResult;
 import uk.ac.bbsrc.tgac.miso.service.security.AuthorizationManager;
 
 @Service
@@ -30,6 +42,9 @@ public class DefaultWorkflowManager implements WorkflowManager {
 
   @Autowired
   ProgressStore progressStore;
+
+  @Autowired
+  BarcodableViewService barcodableViewService;
 
   @Override
   public Workflow beginWorkflow(String workflowName) {
@@ -80,8 +95,67 @@ public class DefaultWorkflowManager implements WorkflowManager {
   }
 
   private ProgressStep makeProgressStep(Set<InputType> inputTypes, String input) {
+    ValidationResult validationResult = new ValidationResult();
+    if (containsBarcodable(inputTypes)) {
+      List<BarcodableView> views = barcodableViewService.searchByBarcode(input, extractEntityTypes(inputTypes));
+      if (views.size() > 0) {
+        validationResult.addError(new ValidationError("Found entities with duplicate barcodes"));
+        validationResult.throwIfInvalid();
+      } else if (views.size() == 1) {
+        return makeProgressStep(views.get(1));
+      } else {
+        if (!containsPrimitive(inputTypes)) {
+          validationResult.addError(new ValidationError("Barcode not found"));
+          validationResult.throwIfInvalid();
+        }
+      }
+    } else if (containsEntity(inputTypes)) {
+      // todo
+    } else {
+      makeProgressStep(getPrimitiveType(inputTypes), input);
+    }
     // todo
     return null;
+  }
+
+  private ProgressStep makeProgressStep(InputType inputType, String input) {
+    // todo
+    return null;
+  }
+
+  private InputType getPrimitiveType(Set<InputType> inputTypes) {
+    // todo
+    return null;
+  }
+
+  private boolean containsEntity(Set<InputType> inputTypes) {
+    // todo
+    return false;
+  }
+
+  private boolean containsPrimitive(Set<InputType> inputTypes) {
+    // todo
+    return false;
+  }
+
+  private ProgressStep makeProgressStep(BarcodableView barcodableView) {
+    // todo
+    return null;
+  }
+
+  private InputType entityTypeToInputType(EntityType targetType) {
+    // todo
+    return null;
+  }
+
+  private Collection<EntityType> extractEntityTypes(Set<InputType> inputTypes) {
+    // todo
+    return null;
+  }
+
+  private boolean containsBarcodable(Set<InputType> inputTypes) {
+    // todo
+    return false;
   }
 
   @Override
