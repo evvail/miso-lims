3d053a89685cd46e6d0194202ea5c0e73306ee2a
Noah Reinhardt <Noah.Reinhardt@oicr.on.ca>
2018-03-28 12:09:27 -0400

    Add documentation to WorkflowManager

--- miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/WorkflowManager.java
+++ miso-service/src/main/java/uk/ac/bbsrc/tgac/miso/service/WorkflowManager.java
@@ -6,17 +6,40 @@ import java.util.List;
 import uk.ac.bbsrc.tgac.miso.core.data.workflow.Workflow;
 
 public interface WorkflowManager {
+  /**
+   * @param workflowName String representation of a WorkflowName
+   */
   Workflow beginWorkflow(String workflowName) throws IOException;
 
+  /**
+   * Similar to processInput(Workflow, int, String), but for workflow's current step
+   */
   Workflow processInput(Workflow workflow, String input) throws IOException;
 
+  /**
+   * Interpret input as part of the step identified by stepNumber.
+   * Delegate input processing to workflow
+   * @param input user input
+   * @param stepNumber 0-based index into workflow steps
+   * @return updated Workflow
+   */
   Workflow processInput(Workflow workflow, int stepNumber, String input) throws IOException;
 
+  /**
+   * Remove the latest step from workflow and any effects it caused
+   * @return update Workflow
+   */
   Workflow cancelInput(Workflow workflow) throws IOException;
 
   Workflow loadProgress(long id) throws IOException;
 
+  /**
+   * @return Workflows owned by the current User
+   */
   List<Workflow> listUserWorkflows() throws IOException;
 
+  /**
+   * todo
+   */
   void execute(Workflow workflow);
 }
